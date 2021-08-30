510f5d2c9a7e421c0d248652a12efa96638194c4
evanchristians <evanryk@gmail.com>
2021-08-30 15:23:16 +0200

    removing some randomness in petal angle calculation

--- src/lib/getAngle.js
+++ src/lib/getAngle.js
@@ -1,7 +1,6 @@
 export const getAngle = (index, total) => {
   return Math.floor(
     (360 * index) / total +
-      10 * (Math.random() * 2 - 1) +
       360 * Math.floor(Math.random() * 2 - 1)
   );
 };
